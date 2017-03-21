3c3836bddef0f15ebcc59145dff237a7f0036007
Michael MacFadden <michael@convergencelabs.com>
2017-03-21 13:38:37 -0700

    Reverts an inadvertent change to the example.

--- example/src/index.js
+++ example/src/index.js
@@ -1,5 +1,4 @@
 const editable = document.getElementById("editable");
-const editable2 = document.getElementById("editable2");
 
 Convergence.connectAnonymously(DOMAIN_URL).then(function (domain) {
   return domain.models().open("content-editable", "test", function () {
@@ -10,15 +9,4 @@ Convergence.connectAnonymously(DOMAIN_URL).then(function (domain) {
 }).then(function (model) {
   const binder = new ConvergenceDomUtils.DomBinder(editable, model.root());
   editable.contentEditable = true;
-});
-
-Convergence.connectAnonymously(DOMAIN_URL).then(function (domain) {
-  return domain.models().open("content-editable", "test2", function () {
-    return ConvergenceDomUtils.DomConverter.htmlToJson(
-      "Here is some initial text with a <b>bold</b> section and some <i>italics</i>."
-    );
-  });
-}).then(function (model) {
-  const binder = new ConvergenceDomUtils.DomBinder(editable2, model.root());
-  editable2.contentEditable = true;
 });
